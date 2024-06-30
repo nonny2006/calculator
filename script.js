186c2e73f4c826b0c391c925a446c597695a88da
Onono Okon <ononookon216@gmail.com>
2024-06-30 12:34:09 -0700

    changes some html and css and added a javascript file

---
+++ script.js
@@ -0,0 +1,29 @@
+let clear = document.getElementById("clear");
+let brackets = document.getElementById("brackets");
+let percentage = document.getElementById("percentage");
+let divide = document.getElementById("divide");
+
+let one = document.getElementById("clear");
+let two = document.getElementById("brackets");
+let three = document.getElementById("percentage");
+let multiply = document.getElementById("divide");
+
+let four = document.getElementById("clear");
+let five = document.getElementById("brackets");
+let six = document.getElementById("percentage");
+let add = document.getElementById("divide");
+
+let seven = document.getElementById("clear");
+let eight = document.getElementById("brackets");
+let nine = document.getElementById("percentage");
+let subtract = document.getElementById("divide");
+
+let decimalPoint = document.getElementById("clear");
+let zero = document.getElementById("brackets");
+let tripleZero = document.getElementById("percentage");
+let result = document.getElementById("divide");
+
+
+one.addEventListener(click, function(){
+    document.getElementById("input");
+})
